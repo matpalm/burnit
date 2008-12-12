7e5f54361b4eefa723c7c8b5363cbad1ad1ffbba
mat <matthew.kelcey@gmail.com>
2008-12-12 22:38:17 +1100

    random test data

--- gen_test_file.rb
+++ gen_test_file.rb
@@ -3,4 +3,4 @@ raise "gen_test_file.rb num_items" unless ARGV.length==1
 lower = 100000
 upper = 1200000
 diff = upper - lower
-ARGV[0].to_i.times { |i|puts "#{((rand() * diff ) + lower).to_i} item.#{i}" }
+ARGV[0].to_i.times { |i|puts "#{((rand() * diff ) + lower).to_i} ./item.#{i}" }
