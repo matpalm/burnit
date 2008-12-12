fa7e2f94f05c1d014b997bbe03ca27c6de31c845
mat <matthew.kelcey@gmail.com>
2008-12-12 22:35:52 +1100

    generate test data

---
+++ gen_test_file.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+raise "gen_test_file.rb num_items" unless ARGV.length==1
+lower = 100000
+upper = 1200000
+diff = upper - lower
+ARGV[0].to_i.times { |i|puts "#{((rand() * diff ) + lower).to_i} item.#{i}" }
